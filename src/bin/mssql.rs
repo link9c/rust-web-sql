a4cbd6c0f01ef7b39dde7c659f439cfb46bf7fd4
chenyulei <chenyulei@microport.com>
2020-09-08 10:57:57 +0800

    delete env

---
+++ src/bin/mssql.rs
@@ -0,0 +1,11 @@
+use serde::{Serialize, Deserialize};
+use sqlx::{FromRow, Row};
+use sqlx::mssql::{Mssql, MssqlRow};
+use anyhow::Result;
+
+
+
+fn main() {
+
+    let database_url = "mssql://link:12345@118.25.135.209/coolq";
+}
